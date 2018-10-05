7cfe73f7ad803d076e0a3dd1341f09f7ec7c801f
Rishabh Tyagi <rty2592@gmail.com>
2018-10-05 00:29:14 -0700

    SecondCommit

--- FinalResults.py
+++ FinalResults.py
@@ -1,98 +1,113 @@
-import pandas as pd
-import numpy as np
-import json
-import io
-import datetime as dt
+# coding=utf-8
+
 import string
-import unicodedata
 import fasttext
-import numpy as np
-from nltk.tokenize import word_tokenize
-import nltk
-from nltk.tokenize.toktok import ToktokTokenizer
-tokenizer = ToktokTokenizer()
-stopword_list = nltk.corpus.stopwords.words('english')
 import re
 from re import sub
-from bs4 import BeautifulSoup
-from gensim import corpora, models, similarities
-from nltk.tokenize import word_tokenize
 from sklearn.decomposition import PCA
 from matplotlib import pyplot
+from io import open
 
-df=pd.read_csv('/Users/RishabhTyagi/Downloads/sim_cancer.csv')
-print('Number of observations are: '+str(len(df)))
+RelevantTweets = open("RelevantTweets.txt","w", encoding='utf-8')
+NonRelevantTweets = open("NonRelevantTweets.txt", "w", encoding='utf-8')
+NonRelevantTweet_dictionary = {}
+RelevantTweet_dictionary = {}
 
-df=df.tweet.dropna()
-df = df.reset_index(drop=True)
-print('Number of observations are: '+str(len(df)))
+with open('/Users/RishabhTyagi/Downloads/sim_fire.csv') as f:
+    i = 0
+    j = 0
+    counter = 0
+    for line in f.readlines():
+        counter+=1
+        if counter <= 1: continue
+        temp = line.strip().split(',')
+        if temp[3] == "":
+            NonRelevantTweets.write(temp[1])
+            NonRelevantTweet_dictionary[i] = temp[1].lower()
+            i+=1
+        else:
+            RelevantTweets.write(temp[1])
+            RelevantTweet_dictionary[j] = temp[1].lower()
+            j+=1
 
-tweet_dictionary = {}
-i = 0
-for line in df:
-        tweet_dictionary[i] = line.lower()
-        i += 1
-
-##### Removes all the links in the tweet
-def strip_links(text):
-    link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
-    links         = re.findall(link_regex, text)
-    for link in links:
-        text = text.replace(link[0], ', ')
-    return text
-
-for i in range(0,len(tweet_dictionary)):
-    tweet_dictionary[i]=strip_links(tweet_dictionary[i])
-
-#### REMOVES ALL THE '@'  AND '#' HANDLE MENTIONS IN THE TWEET - HANDLE AND HASHTAGS
-def strip_all_entities(text):
-    entity_prefixes = ['@','#']
-    for separator in  string.punctuation:
-        if separator not in entity_prefixes :
-            text = text.replace(separator,' ')
-    words = []
-    for word in text.split():
-        word = word.strip().to_bytes(4, 'little')
-        if word:
-            if word[0] not in entity_prefixes:
-                words.append(word)
-    return ' '.join(words)
-
-for i in range(0,len(tweet_dictionary)):
-    tweet_dictionary[i]=strip_links(tweet_dictionary[i])
-
-#remove the header from the df
-df = df.iloc[1:]
-##### Remove the retweet (RT) symbol
-for i in range(0,len(df)):
-    tweet_dictionary[i] = tweet_dictionary[i].replace('RT', '')
-
-##### Remove erroneous words
-
-for i in range(0,len(tweet_dictionary)):
-    tweet_dictionary[i] = sub(pattern=r"\d", repl=r"", string=tweet_dictionary[i])
+def preProcess(tweet_dictionary):
 
 
 
-lines = tweet_dictionary.values()
-tokenizedTweetsList = []
+
+    ##### Removes all the links in the tweet
+    def strip_links(text):
+        link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
+        links         = re.findall(link_regex, text)
+        for link in links:
+            text = text.replace(link[0], ', ')
+        return text
+
+    for i in range(0,len(tweet_dictionary)):
+        tweet_dictionary[i]=strip_links(tweet_dictionary[i])
+
+    #### REMOVES ALL THE '@'  AND '#' HANDLE MENTIONS IN THE TWEET - HANDLE AND HASHTAGS
+    def strip_all_entities(text):
+        entity_prefixes = ['@','#']
+        for separator in  string.punctuation:
+            if separator not in entity_prefixes :
+                text = text.replace(separator,' ')
+        words = []
+        for word in text.split():
+            word = word.strip().to_bytes(4, 'little')
+            if word:
+                if word[0] not in entity_prefixes:
+                    words.append(word)
+        return ' '.join(words)
+
+    for i in range(0,len(tweet_dictionary)):
+        tweet_dictionary[i]=strip_links(tweet_dictionary[i])
+
+    #remove the header from the df
+    #df = df.iloc[1:]
+    ##### Remove the retweet (RT) symbol
+    for i in range(0,len(tweet_dictionary)):
+        tweet_dictionary[i] = tweet_dictionary[i].replace('RT', '')
+
+    ##### Remove erroneous words
+
+    for i in range(0,len(tweet_dictionary)):
+        tweet_dictionary[i] = sub(pattern=r"\d", repl=r"", string=tweet_dictionary[i])
+
+    return tweet_dictionary
+
+NonRelevantTweet_dictionary = preProcess(NonRelevantTweet_dictionary)
+RelevantTweet_dictionary = preProcess(RelevantTweet_dictionary)
 
 
 
-with open("data.txt",'w') as file:
+def writeDicttoFile(tweet_dictionary, keyword):
+    lines = tweet_dictionary.values()
+    with open(keyword+".txt","w") as file:
+        for line in lines:
+            file.write(line)
 
-    for line in lines:
-        file.write(line)
+writeDicttoFile(NonRelevantTweet_dictionary, "NonRelevantTweet")
+writeDicttoFile(RelevantTweet_dictionary,"RelevantTweet")
 
-model = fasttext.skipgram('data.txt', 'model')
 
-print model.words
-X =  model[model.words]
-pca = PCA(n_components=2)
-result = pca.fit_transform(X)
-pyplot.scatter(result[:, 0], result[:, 1])
-words = list(model.words)
-for i, word in enumerate(words):
-    print word, i
-    pyplot.annotate(word, xy=(result[i, 0], result[i, 1]))
+modelsNonRelevant = fasttext.skipgram('NonRelevantTweet.txt', 'model')
+modelsRelevant = fasttext.skipgram('RelevantTweet.txt', 'model')
+
+def calcPCA(tweet_dictionary,model):
+    pca = PCA(n_components=2)
+    X =[]
+    for line in tweet_dictionary.values():
+        X.append(model[line])
+    result = pca.fit_transform(X)
+    return result
+
+resultRelevant = calcPCA(RelevantTweet_dictionary,modelsRelevant)
+resultNonRelevant = calcPCA(NonRelevantTweet_dictionary,modelsNonRelevant)
+
+
+pyplot.scatter(resultRelevant[:, 0], resultRelevant[:, 1], color="blue")
+
+pyplot.scatter(resultNonRelevant[:, 0], resultNonRelevant[:, 1], color = "red")
 pyplot.show()
+
